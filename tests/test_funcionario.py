78e7b8681ca1bc2d5c044def399d3f3865e15390
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 11:10:07 -0300

    Teste 4 funcionario

--- tests/test_funcionario.py
+++ tests/test_funcionario.py
@@ -19,3 +19,13 @@ class TestFuncionario(unittest.TestCase):
         self.assertEqual(1, len(self.__funcionario.projetos))
         self.assertIn(projeto, self.__funcionario.projetos)
     
+    def test_adicionar_multiplos_projetos_no_funcionario(self):
+        projeto1 = Projeto("INE5429")
+        projeto2 = Projeto("INE5455")
+
+        self.__funcionario.adicionar_projeto(projeto1)
+        self.__funcionario.adicionar_projeto(projeto2)
+
+        self.assertEqual(2, len(self.__funcionario.projetos))
+        self.assertIn(projeto1, self.__funcionario.projetos)
+        self.assertIn(projeto2, self.__funcionario.projetos)
