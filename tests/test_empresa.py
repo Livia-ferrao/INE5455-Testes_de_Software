0a58a2f8c9cf9105fd484001a12c953a2fc8ac87
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 10:18:43 -0300

    Refatoracao 1 empresa

--- tests/test_empresa.py
+++ tests/test_empresa.py
@@ -2,15 +2,14 @@ import unittest
 from empresa import Empresa
 
 class TestEmpresa(unittest.TestCase):
+    def setUp(self):
+        self.__empresa = Empresa("W")
+
     def test_criar_empresa_com_nome_valido(self):
-        nome = "W"
-        empresa = Empresa(nome)
-        self.assertEqual(empresa.nome, nome)
+        self.assertEqual(self.__empresa.nome, "W")
         
     def test_criar_empresa_sem_funcionarios(self):
-        empresa = Empresa("W")
-        self.assertEqual(len(empresa.funcionarios), 0)
+        self.assertEqual(len(self.__empresa.funcionarios), 0)
 
     def test_criar_empresa_sem_projetos(self):
-        empresa = Empresa("W")
-        self.assertEqual(len(empresa.projetos), 0)
+        self.assertEqual(len(self.__empresa.projetos), 0)
