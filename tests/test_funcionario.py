d3624cd1e227a454fb335816095f7ba6e0e8bfaf
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 11:09:25 -0300

    Teste 3 funcionario

--- tests/test_funcionario.py
+++ tests/test_funcionario.py
@@ -1,5 +1,6 @@
 import unittest
 from funcionario import Funcionario
+from projeto import Projeto
 
 class TestFuncionario(unittest.TestCase):
     def setUp(self):
@@ -10,3 +11,11 @@ class TestFuncionario(unittest.TestCase):
         
     def test_criar_funcionario_sem_projetos(self):
         self.assertEqual(0, len(self.__funcionario.projetos))
+        
+    def test_adicionar_projeto_no_funcionario(self):
+        projeto = Projeto("INE5429")
+        self.__funcionario.adicionar_projeto(projeto)
+
+        self.assertEqual(1, len(self.__funcionario.projetos))
+        self.assertIn(projeto, self.__funcionario.projetos)
+    
