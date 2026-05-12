942044e4bc9c06d6a1c640f12dd5534dbcdef8ab
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 11:05:28 -0300

    Refatoracao 4 funcionario

--- tests/test_funcionario.py
+++ tests/test_funcionario.py
@@ -2,10 +2,11 @@ import unittest
 from funcionario import Funcionario
 
 class TestFuncionario(unittest.TestCase):
+    def setUp(self):
+        self.__funcionario = Funcionario("José")
+
     def test_criar_funcionario_com_nome_valido(self):
-        nome = "José"
-        func = Funcionario(nome)
-        self.assertEqual(nome, func.nome)
+        self.assertEqual("José", self.__funcionario.nome)
         
     def test_criar_funcionario_sem_projetos(self):
         self.assertEqual(0, len(self.__funcionario.projetos))
