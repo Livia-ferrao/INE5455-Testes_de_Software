25c404f230865246295b3f2633e3626471109a83
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 10:54:59 -0300

    Codigo 3 projeto

--- src/projeto.py
+++ src/projeto.py
@@ -2,3 +2,6 @@ class Projeto:
     def __init__(self, nome):
         self.nome = nome
         self.funcionarios = []
+        
+    def adicionar_funcionario(self, funcionario):
+        self.funcionarios.append(funcionario)
