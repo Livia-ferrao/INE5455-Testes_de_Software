990a001321dd35bf49ac0d54e862b8e590d4237c
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 11:10:27 -0300

    Codigo 5 funcionario

--- src/funcionario.py
+++ src/funcionario.py
@@ -4,4 +4,9 @@ class Funcionario:
         self.projetos = []
         
     def adicionar_projeto(self, projeto):
+        if not projeto:
+            raise ValueError("O projeto não pode ser nulo.")
+        if projeto in self.projetos:
+            raise ValueError("Projeto já está na lista.")
+
         self.projetos.append(projeto)
