f754c6dc71506783146b48c5ec3fecd8da5e3247
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 11:09:49 -0300

    Codigo 3 funcionario

--- src/funcionario.py
+++ src/funcionario.py
@@ -2,3 +2,6 @@ class Funcionario:
     def __init__(self, nome):
         self.nome = nome
         self.projetos = []
+        
+    def adicionar_projeto(self, projeto):
+        self.projetos.append(projeto)
