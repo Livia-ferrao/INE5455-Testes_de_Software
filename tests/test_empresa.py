ae7c8ace7d160228d211c56563c23ab850f232da
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 10:22:30 -0300

    Teste 4 empresa

--- tests/test_empresa.py
+++ tests/test_empresa.py
@@ -1,5 +1,6 @@
 import unittest
 from empresa import Empresa
+from funcionario import Funcionario
 
 class TestEmpresa(unittest.TestCase):
     def setUp(self):
@@ -13,3 +14,9 @@ class TestEmpresa(unittest.TestCase):
 
     def test_criar_empresa_sem_projetos(self):
         self.assertEqual(len(self.__empresa.projetos), 0)
+        
+    def test_adicionar_funcionario_na_empresa(self):
+        funcionario = Funcionario("José")
+        self.__empresa.adicionar_funcionario(funcionario)
+        self.assertEqual(1, len(self.__empresa.funcionarios))
+        self.assertIn(funcionario, self.__empresa.funcionarios)
