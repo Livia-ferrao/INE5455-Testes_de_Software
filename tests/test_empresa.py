6bbaf5e16b5931d185d8e5bdc33013d09ec3ba44
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-12 10:27:29 -0300

    Teste 5 empresa

--- tests/test_empresa.py
+++ tests/test_empresa.py
@@ -20,3 +20,16 @@ class TestEmpresa(unittest.TestCase):
         self.__empresa.adicionar_funcionario(funcionario)
         self.assertEqual(1, len(self.__empresa.funcionarios))
         self.assertIn(funcionario, self.__empresa.funcionarios)
+        
+    def test_adicionar_multiplos_funcionarios_na_empresa(self):
+        funcionario1 = Funcionario("José")
+        funcionario2 = Funcionario("Maria")
+
+        self.__empresa.adicionar_funcionario(funcionario1)
+        self.__empresa.adicionar_funcionario(funcionario2)
+
+        self.assertEqual(2, len(self.__empresa.funcionarios))
+        self.assertIn(funcionario1, self.__empresa.funcionarios)
+        self.assertIn(funcionario2, self.__empresa.funcionarios)
+
+    
