92222bbf5e15b017c6d4d2cf6da8687ed0292d48
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-19 08:46:50 -0300

    Codigo 2 ocorrencia

--- src/ocorrencia.py
+++ src/ocorrencia.py
@@ -1,5 +1,8 @@
 class Ocorrencia:
     def __init__(self, chave, resumo):
+        if not chave:
+            raise ValueError("A chave não pode ser vazia ou nula.")
+
         self.__chave = chave
         self.__resumo = resumo
 
