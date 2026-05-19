c939d2981407e1a5456466addea32ce00e8111d6
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-19 09:37:37 -0300

    Teste inicializacao ocorrencia

--- tests/test_ocorrencia.py
+++ tests/test_ocorrencia.py
@@ -1,19 +1,42 @@
 import unittest
 from ocorrencia import Ocorrencia
-
+from funcionario import Funcionario
+from enums import Prioridade, Tipo
 
 class TestEmpresa(unittest.TestCase):
     def setUp(self):
-        self.__ocorrencia = Ocorrencia(chave="ID1", resumo="Ocorrencia referente ao ID1")
+        self.__jose = Funcionario("José")
+        self.__ocorrencia = Ocorrencia(
+            chave="ID1",
+            resumo="Ocorrencia referente ao ID1",
+            responsavel=self.__jose,
+            prioridade=Prioridade.ALTA,
+            tipo=Tipo.BUG,
+        )
 
-    def test_cria_ocorrencia_com_chave_e_resumo(self):
+    def test_cria_ocorrencia_valida(self):
         self.assertEqual("ID1", self.__ocorrencia.chave)
         self.assertEqual("Ocorrencia referente ao ID1", self.__ocorrencia.resumo)
-        
+        self.assertEqual(self.__jose, self.__ocorrencia.responsavel)
+        self.assertEqual(Prioridade.ALTA, self.__ocorrencia.prioridade)
+        self.assertEqual(Tipo.BUG, self.__ocorrencia.tipo)
+
     def test_nao_cria_ocorrencia_com_chave_vazia(self):
         with self.assertRaises(ValueError):
-            Ocorrencia(chave="", resumo="Nao vazio")
+            Ocorrencia(chave="", resumo="Nao vazio", responsavel=self.__jose, prioridade=Prioridade.BAIXA, tipo=Tipo.BUG)
 
     def test_nao_cria_ocorrencia_com_resumo_vazio(self):
         with self.assertRaises(ValueError):
-            Ocorrencia(chave="ID0", resumo="")
+            Ocorrencia(chave="ID0", resumo="", responsavel=self.__jose, prioridade=Prioridade.BAIXA, tipo=Tipo.BUG)
+
+    def test_nao_cria_ocorrencia_com_responsavel_vazio(self):
+        with self.assertRaises(ValueError):
+            Ocorrencia(chave="ID0", resumo="Nao vazio", responsavel=None, prioridade=Prioridade.BAIXA, tipo=Tipo.BUG)
+
+    def test_nao_cria_ocorrencia_com_prioridade_vazia(self):
+          with self.assertRaises(ValueError):
+            Ocorrencia(chave="ID0", resumo="Nao vazio", responsavel=self.__jose, prioridade=None, tipo=Tipo.BUG)
+
+    def test_nao_cria_ocorrencia_com_tipo_vazio(self):
+        with self.assertRaises(ValueError):
+            Ocorrencia(chave="ID0", resumo="Nao vazio", responsavel=self.__jose, prioridade=Prioridade.BAIXA, tipo=None)
