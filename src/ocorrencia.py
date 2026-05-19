a83b6e24db3fdbd00ae3078dbccb4dddd63f77fc
Livia-ferrao <livia.ferrao16@hotmail.com>
2026-05-19 08:48:08 -0300

    Codigo 3 ocorrencia

--- src/ocorrencia.py
+++ src/ocorrencia.py
@@ -2,6 +2,9 @@ class Ocorrencia:
     def __init__(self, chave, resumo):
         if not chave:
             raise ValueError("A chave não pode ser vazia ou nula.")
+        
+        if not resumo:
+            raise ValueError("O resumo não pode ser vazio ou nulo.")
 
         self.__chave = chave
         self.__resumo = resumo
